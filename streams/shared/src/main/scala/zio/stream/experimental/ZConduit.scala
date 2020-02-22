4c986f0050794dc5c1e0dd7ddcff09a78dc26e97
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-22 13:44:36 +0000

    Add ZStream constructors

--- streams/shared/src/main/scala/zio/stream/experimental/ZConduit.scala
+++ streams/shared/src/main/scala/zio/stream/experimental/ZConduit.scala
@@ -9,11 +9,7 @@ sealed abstract class ZConduit[-R, +E, -I, +O, +Z] private[stream] (
 sealed abstract class ZStream[-R, +E, +O](
   val process: ZManaged[R, Nothing, ZIO[R, Either[E, Unit], Chunk[O]]]
 ) extends ZConduit[R, E, Unit, O, Unit](process.map(pull => _ => pull)) { self =>
-  def map[O2](f: O => O2): ZStream[R, E, O2] =
-    ZStream(self.process.map(_.map(_.map(f))))
-
-  def mapConcat[O2](f: O => Iterable[O2]): ZStream[R, E, O2] =
-    ZStream(self.process.map(_.map(_.flatMap(o => Chunk.fromIterable(f(o))))))
+  import ZStream.Pull
 
   def filter(f: O => Boolean): ZStream[R, E, O] =
     ZStream(self.process.map(_.map(_.filter(f))))
@@ -79,13 +75,76 @@ sealed abstract class ZStream[-R, +E, +O](
       } yield go(outerStream, currOuterChunk, currOuterChunkIdx, finalizer, currInnerStream)
     }
   }
+
+  def map[O2](f: O => O2): ZStream[R, E, O2] =
+    ZStream(self.process.map(_.map(_.map(f))))
+
+  def mapConcat[O2](f: O => Iterable[O2]): ZStream[R, E, O2] =
+    ZStream(self.process.map(_.map(_.flatMap(o => Chunk.fromIterable(f(o))))))
+
+  /**
+   * Takes all elements of the stream for as long as the specified predicate
+   * evaluates to `true`.
+   */
+  final def takeWhile(pred: O => Boolean): ZStream[R, E, O] =
+    ZStream {
+      for {
+        done <- Ref.make(false).toManaged_
+        as   <- self.process
+        pull = done.get.flatMap {
+          if (_)
+            Pull.end
+          else
+            as.flatMap { chunk =>
+              val res = chunk.takeWhile(pred)
+              res.length match {
+                case 0                     => done.set(true) *> Pull.end
+                case n if n < chunk.length => done.set(true) *> Pull.emit(res)
+                case _                     => Pull.emit(res)
+              }
+            }
+        }
+      } yield pull
+    }
 }
 
 object ZStream {
+  object Pull {
+    def emit[A](a: A): IO[Nothing, Chunk[A]]                  = UIO(Chunk.single(a))
+    def emit[A](as: Chunk[A]): IO[Nothing, Chunk[A]]          = UIO(as)
+    def fail[E](e: E): IO[Either[E, Nothing], Nothing]        = IO.fail(Left(e))
+    def halt[E](c: Cause[E]): IO[Either[E, Nothing], Nothing] = IO.halt(c).mapError(Left(_))
+    val end: IO[Either[Nothing, Unit], Nothing]               = IO.fail(Right(()))
+  }
+
   def apply[R, E, O](
     process: ZManaged[R, Nothing, ZIO[R, Either[E, Unit], Chunk[O]]]
   ): ZStream[R, E, O] =
     new ZStream(process) {}
+
+  /**
+   * The stream that always dies with the `ex`.
+   *
+   * @param ex The exception that kills the stream
+   * @return a stream that dies with an exception
+   */
+  def die(ex: => Throwable): UStream[Nothing] =
+    halt(Cause.die(ex))
+
+  /**
+   * The stream that always dies with an exception described by `msg`.
+   *
+   * @param msg The message to feed the runtime exception
+   * @return a stream that dies with a runtime exception
+   */
+  def dieMessage(msg: => String): UStream[Nothing] =
+    halt(Cause.die(new RuntimeException(msg)))
+
+  /**
+   * The empty stream
+   */
+  val empty: UStream[Nothing] =
+    ZStream(Managed.succeedNow(Pull.end))
 
   /**
    * Creates a stream from a [[zio.Chunk]] of values
@@ -95,7 +154,29 @@ object ZStream {
    * @return a finite stream of values
    */
   def fromChunk[O](c: => Chunk[O]): ZStream[Any, Nothing, O] =
-    ZStream(Managed.succeed(UIO.succeed(c)))
+    ZStream(Managed.succeedNow(UIO.succeed(c)))
+
+  /**
+   * The stream that always halts with `cause`.
+   *
+   * @tparam The error type
+   * @param the cause for halting the stream
+   * @return a stream that is halted
+   */
+  def halt[E](cause: => Cause[E]): Stream[E, Nothing] =
+    ZStream(Managed.succeedNow(Pull.halt(cause)))
+
+  /**
+   * The infinite stream of iterative function application: a, f(a), f(f(a)), f(f(f(a))), ...
+   */
+  def iterate[A](a: A)(f: A => A): UStream[A] =
+    ZStream {
+      Managed.fromEffect {
+        Ref.make(a).map { currA =>
+          currA.modify(a => Chunk.single(a) -> f(a))
+        }
+      }
+    }
 
   /**
    * Creates a single-valued stream from a managed resource
@@ -119,6 +200,21 @@ object ZStream {
         }
       } yield pull
     }
+
+  /**
+   * The stream that never produces any value or fails with any error.
+   */
+  val never: UStream[Nothing] =
+    ZStream(ZManaged.succeedNow(UIO.never))
+
+  /**
+   * Constructs a stream from a range of integers (lower bound included, upper bound not included)
+   *
+   * @param min the lower bound
+   * @param max the upper bound
+   */
+  def range(min: Int, max: Int): UStream[Int] =
+    iterate(min)(_ + 1).takeWhile(_ < max)
 }
 
 sealed abstract class ZSink[-R, +E, -I, +Z](
